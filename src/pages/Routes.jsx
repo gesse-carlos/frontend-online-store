e77f2692b90650d50ffc5022fba57f85a489506f
Gessé Carlos <gesse.dev@outlook.com>
2021-11-18 04:45:57 -0300

    pages created

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Home } from '.';
+import { Home, Cart, ProductDetails } from '.';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
+      <Route path="/cart" component={ Cart } />
+      <Route path="/product=:id" component={ ProductDetails } />
     </Switch>
   );
 }
