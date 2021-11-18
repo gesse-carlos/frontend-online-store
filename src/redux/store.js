b5602cf4d83e56746bcfd64f422013db06f2868e
Gessé Carlos <gesse.dev@outlook.com>
2021-11-18 04:41:54 -0300

    store setup finished

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+import { shopSlice, cartSlice } from "./reducers";
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    shop: shopSlice.reducer,
+    cart: cartSlice.reducer,
+  },
 }, composeWithDevTools());
 
 export default store;
